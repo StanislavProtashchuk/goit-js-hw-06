74cf42c0238f8847da20b11cb5ad79b3d5ada0a0
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-01-13 14:06:18 +0200

    secondDone

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const titleOfIngredients = document.querySelector('#ingredients');
+
+const listOfIngredients = ingredients.forEach(element => {
+  const ingredient = document.createElement('li');
+  ingredient.textContent = element;
+
+  titleOfIngredients.append(ingredient);
+})
+
+
+
+

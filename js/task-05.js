9cd27c74245cff5ee2d1e42d7fc302c6172bef3b
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-01-14 12:18:03 +0200

    Task5Done

---
+++ js/task-05.js
@@ -0,0 +1,20 @@
+const staticValue = 'Anonymous'
+
+const refs = {
+input: document.querySelector('#name-input'),
+nameLabel: document.querySelector('#name-output'),
+};
+
+refs.input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    console.log(event.currentTarget.value);
+
+refs.nameLabel.textContent = staticValue;
+    if (event.currentTarget.value) {
+        refs.nameLabel.textContent = event.currentTarget.value;
+    }
+    }      
+
+
+

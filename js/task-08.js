f7ae0753694e07c2c2c335cec22ab1f3a39fa724
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-01-15 12:41:12 +0200

    Task8Done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+    
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+
+    const { elements: { email, password } } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!');
+    }
+
+    formData.forEach((value, name) => {
+        console.log('name:', name);
+        console.log('value:', value);
+    })
+
+    event.currentTarget.reset();
+   } 

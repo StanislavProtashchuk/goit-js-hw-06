c7d804edb654ec54a4225566cb51d948a2998224
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-01-15 14:01:07 +0200

    task9Done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+
+const body = document.querySelector('body');
+const onButtonEvent = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+
+onButtonEvent.addEventListener('click', changeColor);
+
+function changeColor(event) {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = body.style.backgroundColor;
+  console.log(body.style.backgroundColor);
+}
+
